b9b4bce826fcc8b859080e2f5a7f1a538280cdea
Oleighton <57159834+Oleighton@users.noreply.github.com>
2022-03-17 13:26:27 -0700

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -7,8 +7,8 @@ def send_text_message(destination: str, message:str):
 
     # Find your Account SID and Auth Token at twilio.com/console
     # and set the environment variables. See http://twil.io/secure
-    account_sid = account_sid = 'ACaa4d81455eccc46157065eec13945e8a'
-    auth_token = '5dd48c57f9d5789c80b5243378e323b1'
+    account_sid = account_sid 
+    auth_token = auth_token
 
     client = Client(account_sid, auth_token)
 
